9284f67c33da4b62b8dceb187c9a70dafd045e67
boris <bova4fa@gmail.com>
2023-02-10 20:49:23 +0300

    Sem1Task0 input squared

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача №0
+//квадрат введенного числа
+
+
+Console.Write("Введите число: ");
+//read from console
+string? inputNum = Console.ReadLine(); //класс консоль, метод ReadLine
+
+//check the value is not empty
+if(inputNum != null)
+{
+    //parsing of input value
+    int number = int.Parse(inputNum);
+
+    //find the pw
+ //   int outNum = number*number;
+ //   int outNum = (int)Math.Pow(number, 2);
+
+    //output
+ //   Console.WriteLine("Квадрат числа: "+outNum);
+
+    Console.WriteLine("Квадрат числа: "+(int)Math.Pow(number, 2));
+}
+
