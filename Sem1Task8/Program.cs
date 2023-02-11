24d5056c35c6308816f915b5ca917bdff0e9e00c
boris <bova4fa@gmail.com>
2023-02-11 13:02:47 +0300

    Task 8 added

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 1)
+{
+    for(int i=2; i<=N; i+=2) Console.Write(i + " ");
+    Console.Write("\n");
+} else Console.WriteLine("Введите число > 1!");
+
