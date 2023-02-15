65a5b19b715485bbca2f1a245f4aa9628bf45399
boris <bova4fa@gmail.com>
2023-02-15 13:01:46 +0300

    Homework Seminar 2

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()??"0");
+
+if(num>99)
+{
+    int digitsNum = 0;
+    int temp = num;//service int variable for temporary use
+    while(temp>=1) //counts total number of digits
+    {
+        temp/=10;
+        digitsNum++;
+    }
+
+    temp = Convert.ToInt32(Math.Pow(10, digitsNum-3));//compose the divider till 3rd digit
+    Console.WriteLine($"Третья цифра числа: {num/temp%10}");
+} else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+var daysOfWeek = new Dictionary <int, string> ()
+{
+    [1] = "нет",
+    [2] = "нет",
+    [3] = "нет",
+    [4] = "нет",
+    [5] = "нет",
+    [6] = "да",
+    [7] = "да"
+};
+
+Console.Write("Введите номер дня недели: ");
+int dayWeekNum = int.Parse(Console.ReadLine()??"0");
+
+if(dayWeekNum>0 && dayWeekNum<8)
+{
+  Console.WriteLine(daysOfWeek[dayWeekNum]);
+} else
+{
+    Console.WriteLine("Это не номер дня недели! Введите число от 1 до 7.");
+}
+
