e5fcd0d57e630d3b0ca82d04465b4335cd61a186
boris <bova4fa@gmail.com>
2023-02-18 23:07:14 +0300

    Seminar 3 - Homework

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// *Сделать вариант через СЛОВАРЬ четырехзначных палиндромов
+
+Dictionary <int, string> palindBase = new Dictionary<int, string>();
+
+//method to read data from console and return an integer
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return(int.Parse(Console.ReadLine()??"0"));
+}
+
+//fulfills the dictionary with all 4-digits palindromes and displays them upon request (ifShow!=0)
+void FillPalindrome4Dgt(Dictionary <int, string> palindromes, int ifShow)
+{
+    int cnt = 0;
+    for(int i=1; i<10; i++)
+    {
+        for(int j=0; j<10; j++)
+        {
+            cnt++;
+            palindromes.Add(i*1000 + j*100 + j*10 + i, $"палиндром № {cnt}");
+        }
+    }
+
+    //output the dictionary to console, if requested
+    if(ifShow!=0)
+    {
+        foreach(var item in palindromes)
+        {
+            Console.WriteLine($"key: {item.Key}  value: {item.Value}");
+        }
+    }
+}
+
+//takes 5-digit value, deletes the center digit and return 4-digit value
+int DeleteCenterDigitOf5(int num)
+{
+    return (num/1000)*100 + (num/10%10)*10 + num%10;
+}
+
+//returns the number of digits in input value
+int NumberOfDigits(int num)
+{
+    return (int)(Math.Floor(Math.Log10(num))) + 1;
+}
+
+
+
+int num = ReadData("Введите пятизначное число: ");
+//int digitsNum = (int)(Math.Floor(Math.Log10(num))) + 1;//gets the number of digits
+
+if(NumberOfDigits(num)==5)
+{
+    FillPalindrome4Dgt(palindBase, 0);
+ 
+    if(palindBase.ContainsKey(DeleteCenterDigitOf5(num)))
+    {
+        Console.WriteLine("Это палиндром!!!");
+    } else
+    { 
+        Console.WriteLine("Это НЕ палиндром!");
+    }
+} else
+{
+    Console.WriteLine("Это не пятизначное число, попробуйте еще!");
+}
+
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+// method to read from the user
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return(int.Parse(Console.ReadLine()??"0"));
+}
+
+//output the answer
+void PrintData(string msg, double value)
+{
+    Console.WriteLine(msg + value);
+}
+
+//to find the distance <-> 2 points in 3D coordinates
+double CalcLen3D(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+}
+
+int x1 = ReadData("Ведите координату Х точки №1: ");
+int y1 = ReadData("Ведите координату Y точки №1: ");
+int z1 = ReadData("Ведите координату Z точки №1: ");
+int x2 = ReadData("Ведите координату Х точки №2: ");
+int y2 = ReadData("Ведите координату Y точки №2: ");
+int z2 = ReadData("Ведите координату Z точки №2: ");
+
+Console.WriteLine();
+PrintData("Расстояние между точками: ", CalcLen3D(x1, y1, z1, x2, y2, z2));
