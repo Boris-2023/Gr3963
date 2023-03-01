ef718c55f42ec31f27aa8037d63a19e13694e812
boris <bova4fa@gmail.com>
2023-03-01 17:29:18 +0300

    Seminar 6 - Homework, 01/03/2023

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,109 @@
+﻿// Задача 41: *Пользователь вводит число нажатий, затем программа следит за нажатиями и выдает сколько чисел больше 0 было введено.
+
+int ReadDataToInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+//output the result
+void PrintAnswer(string explain, int num)
+{
+    Console.WriteLine("\n" + explain + num);
+}
+
+//prints 1D array into console
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++) Console.Write($"{arr[i]}, ");
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+//forms a string of preset length from console
+string KeyInputString(int strLen)
+{
+    string res = string.Empty;
+    int cnt = 0;
+
+    while (cnt < strLen)
+    {
+        res = res + Console.ReadKey().KeyChar;
+        cnt++;
+    }
+
+    return res;
+}
+
+//derives all dec values from a string
+int[] ValuesFromString(string inputStr)
+{
+    int[] figs = new int[inputStr.Length];//will store 1 for digits & 0 for non-digits 
+
+    //within figs[]: mark digits with 1, others 0; & count a number of values included
+    int valCnt = 0;
+    for (int i = 0; i < figs.Length; i++)
+    {
+        if (Char.IsDigit(inputStr, i))
+        {
+            figs[i] = 1;
+            if (i == 0 || (i > 0 && figs[i - 1] == 0)) valCnt++;//counts number of new values found
+        }
+        else figs[i] = 0;
+    }
+
+    if (valCnt == 0) return figs;//if no digits found at all - TODO: special return with warning
+
+    //forming the very values by temp string
+    int[] res = new int[valCnt];//array for values found
+    int cnt = 0;
+    bool ifFound = false;//flag - within value
+    string temp = string.Empty;
+
+    valCnt = 0;
+    while (cnt < figs.Length)
+    {
+        if (figs[cnt] == 1)//current symbol is a digit
+        {
+            if (cnt > 0 && inputStr[cnt - 1] == '-') temp = temp + '-';//adjust for negative value
+            temp = temp + inputStr[cnt];
+            ifFound = true;
+        }
+        else
+        {
+            if (ifFound)
+            {
+                res[valCnt] = int.Parse(temp);
+                valCnt++;
+                temp = string.Empty;
+                ifFound = false;
+            }
+        }
+        cnt++;
+    }
+    if (ifFound) res[valCnt] = int.Parse(temp);//if the string is ended by digit
+
+    return res;
+}
+
+//counts all positive values in the array
+int NumPositiveInArr(int[] arr)
+{
+    int cnt = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) cnt++;
+    }
+    return cnt;
+}
+
+
+int tapsNum = ReadDataToInt("Enter a number of keystrokes to take: ");
+Console.Write("\nEnter your string (will interupt automatically): ");
+string inputStr = KeyInputString(tapsNum);//takes a string from console
+
+int[] values = ValuesFromString(inputStr);//derives all values from a string entered
+Console.Write("\nThe values in the string are: ");
+Print1DArr(values);
+
+PrintAnswer("Number of positive values entered: ", NumPositiveInArr(values));//number of positive values
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// * Найдите площадь треугольника образованного пересечением 3 прямых
+
+//takes double value from console; replaces ',' with '.' if found
+double ReadDataToDbl(string msg)
+{
+    Console.Write(msg);
+    string inStr = Console.ReadLine() ?? "0";
+
+    inStr = inStr.Replace(',', '.');
+
+    return (double.Parse(inStr));
+}
+
+//output the result
+void PrintAnswerDbl(string explain, double num, int roundDigits)
+{
+    Console.WriteLine(explain + Math.Round(num, roundDigits));
+}
+
+double[] TwoLinesCrossCoord(double b1, double k1, double b2, double k2)
+{
+    double[] coord = new double[2];
+
+    coord[0] = (b2 - b1) / (k1 - k2);//x
+    coord[1] = (k1 * b2 - k2 * b1) / (k1 - k2);//y
+
+    return coord;
+}
+
+double TwoPtsDist(double x1, double y1, double x2, double y2)
+{
+    return (Math.Pow((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2), .5));
+}
+
+double TriangleSquareBySides(double side1, double side2, double side3)
+{
+    double pHalf;
+
+    pHalf = (side1 + side2 + side3)/2;
+    return Math.Pow(pHalf*(pHalf-side1)*(pHalf-side2)*(pHalf-side3), .5);
+
+}
+
+//Inputs of lines parametres
+double b1 = ReadDataToDbl("Enter b1: ");
+double k1 = ReadDataToDbl("Enter k1: ");
+double b2 = ReadDataToDbl("Enter b2: ");
+double k2 = ReadDataToDbl("Enter k2: ");
+double b3 = ReadDataToDbl("Enter b3: ");
+double k3 = ReadDataToDbl("Enter k3: ");
+
+//finds crossing points
+double[] crd12 = TwoLinesCrossCoord(b1, k1, b2, k2);
+double[] crd13 = TwoLinesCrossCoord(b1, k1, b3, k3);
+double[] crd23 = TwoLinesCrossCoord(b2, k2, b3, k3);
+
+//find3 sides of triangle
+double side1 = TwoPtsDist(crd12[0], crd12[1], crd13[0], crd13[1]);
+double side2 = TwoPtsDist(crd12[0], crd12[1], crd23[0], crd23[1]);
+double side3 = TwoPtsDist(crd13[0], crd13[1], crd23[0], crd23[1]);
+
+//finds the square of triangle
+PrintAnswerDbl("The square of triangle is: ", TriangleSquareBySides(side1,side2,side3), 3);
+
