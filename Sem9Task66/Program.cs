acb1f307e91f6da652cd62605eb3ebe334adc69a
boris <bova4fa@gmail.com>
2023-03-11 19:07:02 +0300

    Seminar 9 RECURSION - Homework, 11 Mar 2023

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//takes data from console in reply to welcome msg & returns input as Int32
+int ReadDataToInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+// computes the sum of natural values in a range [m -> n]
+int SumRecRange(int m, int n)
+{
+    if (m == n)//stop recursion
+    {
+        return n;
+    }
+    else
+    {
+        return m + SumRecRange(m + 1, n);
+    }
+}
+
+//output the result
+void PrintAnswer(string explain, int num)
+{
+    Console.WriteLine("\n" + explain + num);
+}
+
+
+int num1 = ReadDataToInt("Enter number to start : ");
+int num2 = ReadDataToInt("Enter number to finish: ");
+
+int sum = num1 < num2 ? SumRecRange(num1, num2) : SumRecRange(num2, num1);
+PrintAnswer("The sum is: ", sum);
+
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+//takes data from console in reply to welcome msg & returns input as Int32
+int ReadDataToInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+
+// computes the output of Akkerman function for args m & n
+long AkkermanRec(long m, long n)
+{
+    if (m == 0)//stop recursion
+        return (n + 1);
+    else
+      if ((m != 0) && (n == 0))
+        return AkkermanRec(m - 1, 1);
+    else
+        return AkkermanRec(m - 1, AkkermanRec(m, n - 1));
+}
+
+long num1 = (long) ReadDataToInt("Enter value #1 (m): ");
+long num2 = (long) ReadDataToInt("Enter value #2 (n): ");
+
+Console.WriteLine($"\nA({num1},{num2}) = {AkkermanRec(num1, num2)}");
+
